3b01d4aab2203c44c06b03848d4a766896b4f991
Mohammed-Azad <mhammadazad2016@gmail.com>
2024-06-25 05:57:33 +0300

    Initial Commit

---
+++ Simple_WebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple_WebBrowser
+//
+//  Created by Mohammed's MacBook on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
